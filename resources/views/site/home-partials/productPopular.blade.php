26686bc5ff483b9237addde37c4838cad6299b8c
Ashiqul Islam Emu <ashiqulemu.jpi@gmail.com>
2020-10-13 20:47:52 +0600

    # fix

--- resources/views/site/home-partials/productPopular.blade.php
+++ resources/views/site/home-partials/productPopular.blade.php
@@ -9,28 +9,54 @@
         <div class="swiper-container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    <product-card></product-card>
+                    <div class="product">
+                        <div class="photo">
+                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
+                        </div>
+                        <div class="base">
+                            <p class="title">Green Guava</p>
+                            <div class="inner">
+                                <div class="weight">500mg</div>
+                                <div class="price">10 Tk</div>
+                            </div>
+                            <div class="addCart">
+                                <button class="addTocart"  >
+                        <span class="item">
+                           10
+                        </span>
+                                    <i class="fa fa-shopping-cart mr-2"></i>
+                                    Add To Cart
+
+                                </button>
+                                <button class="decr fa fa-minus" ></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="swiper-slide">
-                    <product-card></product-card>
-                </div>
-                <div class="swiper-slide">
-                    <product-card></product-card>
-                </div>
-                <div class="swiper-slide">
-                    <product-card></product-card>
-                </div>
-                <div class="swiper-slide">
-                    <product-card></product-card>
-                </div>
-                <div class="swiper-slide">
-                    <product-card></product-card>
-                </div>
-                <div class="swiper-slide">
-                    <product-card></product-card>
-                </div>
-                <div class="swiper-slide">
-                    <product-card></product-card>
+                    <div class="product">
+                        <div class="photo">
+                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
+                        </div>
+                        <div class="base">
+                            <p class="title">Green Guava</p>
+                            <div class="inner">
+                                <div class="weight">500mg</div>
+                                <div class="price">10 Tk</div>
+                            </div>
+                            <div class="addCart">
+                                <button class="addTocart"  >
+                        <span class="item">
+                           10
+                        </span>
+                                    <i class="fa fa-shopping-cart mr-2"></i>
+                                    Add To Cart
+
+                                </button>
+                                <button class="decr fa fa-minus" ></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
--- resources/views/site/home-partials/products.blade.php
+++ resources/views/site/home-partials/products.blade.php
@@ -70,16 +70,104 @@
             <hr>
             <div class="row mt-5">
                 <div class="col">
-                    <product-card></product-card>
+                    <div class="product">
+                        <div class="photo">
+                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
+                        </div>
+                        <div class="base">
+                            <p class="title">Green Guava</p>
+                            <div class="inner">
+                                <div class="weight">500mg</div>
+                                <div class="price">10 Tk</div>
+                            </div>
+                            <div class="addCart">
+                                <button class="addTocart"  >
+                        <span class="item">
+                           10
+                        </span>
+                                    <i class="fa fa-shopping-cart mr-2"></i>
+                                    Add To Cart
+
+                                </button>
+                                <button class="decr fa fa-minus" ></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    <product-card></product-card>
+                    <div class="product">
+                        <div class="photo">
+                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
+                        </div>
+                        <div class="base">
+                            <p class="title">Green Guava</p>
+                            <div class="inner">
+                                <div class="weight">500mg</div>
+                                <div class="price">10 Tk</div>
+                            </div>
+                            <div class="addCart">
+                                <button class="addTocart"  >
+                        <span class="item">
+                           10
+                        </span>
+                                    <i class="fa fa-shopping-cart mr-2"></i>
+                                    Add To Cart
+
+                                </button>
+                                <button class="decr fa fa-minus" ></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    <product-card></product-card>
+                    <div class="product">
+                        <div class="photo">
+                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
+                        </div>
+                        <div class="base">
+                            <p class="title">Green Guava</p>
+                            <div class="inner">
+                                <div class="weight">500mg</div>
+                                <div class="price">10 Tk</div>
+                            </div>
+                            <div class="addCart">
+                                <button class="addTocart"  >
+                        <span class="item">
+                           10
+                        </span>
+                                    <i class="fa fa-shopping-cart mr-2"></i>
+                                    Add To Cart
+
+                                </button>
+                                <button class="decr fa fa-minus" ></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    <product-card></product-card>
+                    <div class="product">
+                        <div class="photo">
+                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
+                        </div>
+                        <div class="base">
+                            <p class="title">Green Guava</p>
+                            <div class="inner">
+                                <div class="weight">500mg</div>
+                                <div class="price">10 Tk</div>
+                            </div>
+                            <div class="addCart">
+                                <button class="addTocart"  >
+                        <span class="item">
+                           10
+                        </span>
+                                    <i class="fa fa-shopping-cart mr-2"></i>
+                                    Add To Cart
+
+                                </button>
+                                <button class="decr fa fa-minus" ></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
