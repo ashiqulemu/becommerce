04c44c0038c5e2002b2a15104904ce1173352af8
amostafasyed <amostafasyed@gnail.com>
2020-10-13 21:29:30 +0600

    ghghgh

--- resources/views/site/home-partials/productNewArrival.blade.php
+++ resources/views/site/home-partials/productNewArrival.blade.php
@@ -4,19 +4,24 @@
             NEW ARRIVAL
         </h1>
     </div>
+
     <div class="col-md-12 ">
+
         <div class="swiper-container">
+
             <div class="swiper-wrapper">
+                @foreach($latest as $product)
                 <div class="swiper-slide">
+
                     <div class="product">
                         <div class="photo">
-                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
+                            <img src="{{url('images/products/'.$product->product_image) }}"  />
                         </div>
                         <div class="base">
-                            <p class="title">Green Guava</p>
+                            <p class="title">{{$product->name}}</p>
                             <div class="inner">
-                                <div class="weight">500mg</div>
-                                <div class="price">10 Tk</div>
+                                <div class="weight">{{$product->meta_tag}}</div>
+                                <div class="price">{{$product->price}}</div>
                             </div>
                             <div class="addCart">
                                 <button class="addTocart"  >
@@ -31,38 +36,18 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="product">
-                        <div class="photo">
-                            <img src="http://pngimg.com/uploads/guava/guava_PNG57.png">
-                        </div>
-                        <div class="base">
-                            <p class="title">Green Guava</p>
-                            <div class="inner">
-                                <div class="weight">500mg</div>
-                                <div class="price">10 Tk</div>
-                            </div>
-                            <div class="addCart">
-                                <button class="addTocart"  >
-                        <span class="item">
-                           10
-                        </span>
-                                    <i class="fa fa-shopping-cart mr-2"></i>
-                                    Add To Cart
 
-                                </button>
-                                <button class="decr fa fa-minus" ></button>
-                            </div>
-                        </div>
-                    </div>
                 </div>
+                @endforeach
 
             </div>
+
             <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div>
             <!-- Add Pagination -->
 
         </div>
+
     </div>
+
 </section>
