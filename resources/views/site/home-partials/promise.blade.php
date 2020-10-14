d30d64c8dd3239270e42eb2d33d849364ac037d6
ashiqulemucse <ashiqulemu.cse@gmial.com>
2020-10-14 22:26:12 +0600

    user header done

--- resources/views/site/home-partials/promise.blade.php
+++ resources/views/site/home-partials/promise.blade.php
@@ -9,21 +9,21 @@
             <div class="col-md-4">
                 <div class="add">
                     <a href="#">
-                        <img class="img-fluid" src="{{asset('https://previews.123rf.com/images/iqoncept/iqoncept1502/iqoncept150200053/36595722-best-service-words-on-a-business-card-stack-or-pile-with-terms-describing-your-business-including-qu.jpg')}}" alt="">
+                        <img style="height: 200px; " class="img-fluid" src="{{asset('https://previews.123rf.com/images/iqoncept/iqoncept1502/iqoncept150200053/36595722-best-service-words-on-a-business-card-stack-or-pile-with-terms-describing-your-business-including-qu.jpg')}}" alt="">
                     </a>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="add">
                     <a href="#">
-                        <img class="img-fluid" src="{{asset('https://www.pngkit.com/png/detail/50-509907_our-100-satisfaction-guarantee-100-satisfaction-guarantee-logo.png')}}" alt="">
+                        <img style="height: 200px; " class="img-fluid" src="{{asset('https://www.pngkit.com/png/detail/50-509907_our-100-satisfaction-guarantee-100-satisfaction-guarantee-logo.png')}}" alt="">
                     </a>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="add">
                     <a href="#">
-                        <img class="img-fluid" src="{{asset('https://www.ultracourierbd.com/ULTRA-Soft/item_picture_front/1582375790.jpg')}}" alt="">
+                        <img style="height: 200px; " class="img-fluid" src="{{asset('https://www.ultracourierbd.com/ULTRA-Soft/item_picture_front/1582375790.jpg')}}" alt="">
                     </a>
                 </div>
             </div>
