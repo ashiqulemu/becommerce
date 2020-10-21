ccfa1c92e363dd76053f083efb102410cc3b57fe
Ashiqul Islam Emu <ashiqulemu.jpi@gmail.com>
2020-10-21 13:02:55 +0600

    working on header

--- resources/views/site/home-partials/header.blade.php
+++ resources/views/site/home-partials/header.blade.php
@@ -1,24 +1,24 @@
 <section class="site-header">
     @if(!auth()->user())
-    <div class="topbar">
-        <div class="barInner">
-            <div class="items">
-                <i class="fa fa-envelope-o" aria-hidden="true"></i> admin@admin.com
-            </div>
-            <div class="items">
-                <div><i class="fa fa-phone"></i> +880 1723096437</div>
-                <div class="social">
-                    <a href="#"> <i class="fa fa-facebook"></i> </a>
-                    <a href="#"> <i class="fa fa-twitter"></i> </a>
-                    <a href="#"> <i class="fa fa-youtube"></i> </a>
-                    <a href="#"> <i class="fa fa-pinterest"></i> </a>
-                </div>
-            </div>
-            <div class="items">
-                <a href="{{url('/register')}}" class="theme-link"> <i class="fa fa-user"></i> Sign Up </a>
-            </div>
-        </div>
-    </div>
+    {{--<div class="topbar">--}}
+        {{--<div class="barInner">--}}
+            {{--<div class="items">--}}
+                {{--<i class="fa fa-envelope-o" aria-hidden="true"></i> admin@admin.com--}}
+            {{--</div>--}}
+            {{--<div class="items">--}}
+                {{--<div><i class="fa fa-phone"></i> +880 1723096437</div>--}}
+                {{--<div class="social">--}}
+                    {{--<a href="#"> <i class="fa fa-facebook"></i> </a>--}}
+                    {{--<a href="#"> <i class="fa fa-twitter"></i> </a>--}}
+                    {{--<a href="#"> <i class="fa fa-youtube"></i> </a>--}}
+                    {{--<a href="#"> <i class="fa fa-pinterest"></i> </a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="items">--}}
+                {{--<a href="{{url('/register')}}" class="theme-link"> <i class="fa fa-user"></i> Sign Up </a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
     <div class="others">
         <div class="brand">
@@ -34,6 +34,7 @@
                     <form method="POST" action="{{ url('/admin/login') }}" class="form-inline">
                         @csrf
                         <div class="authPanel" id="login-area">
+                           <div class="titleBar"> <article>Already a customer ?</article> </div>
                             <div class="loged_area">
                                 <div>
                                     <input id="login"
@@ -76,6 +77,12 @@
                                         <button type="submit" class="btn  login  ">Login</button>
                                     </div>
                                 </div>
+                                <div class="links">
+                                    <a href="{{ url('auth/facebook') }}" class="btn  btn-sm"  >
+                                        <span>Login with </span> <i class="fa fa-facebook"></i>
+                                    </a>
+
+                                </div>
                             </div>
                             <div class="middle-bottom">
                                 <div>
@@ -95,20 +102,22 @@
         </div>
 
         <div class="cart">
+            <div class="signup">
+                <div class="titleBar"> <article>New to Adi ?</article> </div>
+                <a href="{{url('/register')}}" class="btn  btn-sm"> <i class="fa fa-user"></i> Sign Up </a>
+            </div>
             <div class="inner">
                 <div class="social">
+                    <div class="user">
+                       <span> hi Guest</span>
+                    </div>
                     <div class="mb-3 homeCart mt-3">
                         <a href="/view-cart" title="view shopping cart" class="shoppingCart">
                             <i class="fa fa-cart-arrow-down"  ></i>
                             <div class="counter">{{Cart::content()->count()}}</div>
                         </a>
                     </div>
-                    <div class="links">
-                        <a href="{{ url('auth/facebook') }}" class="btn  btn-sm"  >
-                           <span>Login with </span> <i class="fa fa-facebook-square"></i>
-                        </a>
 
-                    </div>
                 </div>
             </div>
         </div>
