3f5efd1f431756ec55a184d82a200ea07055854c
ashiqulemucse <ashiqulemu.cse@gmial.com>
2020-10-24 17:21:49 +0600

    working on checkout

--- resources/views/admin/pages/agent/create.blade.php
+++ resources/views/admin/pages/agent/create.blade.php
@@ -2,43 +2,43 @@
 
 @section('content')
 
-    <div id="page-wrapper">
-        <br>
+    <div id="page-wrapper" style="overflow-x: hidden">
 
-        <div class="container">
+        <div class="row site-forms mt-4">
+            @if (isset($errors) && count($errors))
 
-            <div class="row justify-content-center my-3">
-                <div class="col-12 col-md-9 form-register p-5">
-                    @if (isset($errors) && count($errors))
+                @foreach($errors->all() as $error)
+                    <h6 align="center" style="color:darkred">
+                        <li>{{$error }}</li>
+                    </h6>
+                @endforeach
+            @endif
 
-                        @foreach($errors->all() as $error)
-                            <h6 align="center" style="color:darkred">   <li>{{$error }}</li> </h6>
-                        @endforeach
-                    @endif
-                    <div class="row ">
-                        <div class="col text-center">
-                            <h2>Create Agent</h2>
-
-                        </div>
-
+                <div class="form-box-header">
+                        + Agent
                     </div>
-                    <form class="form-horizontal manipulate" method="POST" action="{{ url('/admin/agent') }}">
-                        @csrf
-                        <div class="row align-items-center">
-                            <div class="col mt-4">
+            <form class="form-horizontal manipulate" method="POST" action="{{ url('/admin/agent') }}">
+                @csrf
+
+                <div class="col-md-12 mt-2">
+                    <div class="col-md-3">
+                        <div class="form-group ">
+
                                 <input type="text" class="form-control @error('sign_username') is-invalid
                                        @enderror" name="sign_username" value="{{ old('sign_username') }}"
-                                       required  placeholder="User Name">
-                            </div>
+                                       required placeholder="User Name">
+
                             @error('sign_username')
                             <div class="invalid-feedback text-right" role="alert">
                                 <strong>{{ $message }}</strong>
                             </div>
                             @enderror
                         </div>
-                        <div class="row align-items-center mt-4">
-                            <div class="col">
-                                <input type="email"  class="form-control @error('sign_email') is-invalid
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group ">
+
+                                <input type="email" class="form-control @error('sign_email') is-invalid
                                        @enderror" name="sign_email" value="{{ old('sign_email') }}"
                                        required autocomplete="sign_email" placeholder="E-mail Address">
                                 @error('sign_email')
@@ -46,14 +46,20 @@
                                     <strong>{{ $message }}</strong>
                                 </div>
                                 @enderror
-                            </div><br>
-                            <div class="col">
+
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group ">
+
                                 <input type="number" class="form-control" name="mobile"
                                        value="{{ old('moible') }}" placeholder="Phone/Mobile no">
-                            </div>
+
                         </div>
-                        <div class="row align-items-center mt-4">
-                            <div class="col">
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group ">
+
                                 <input type="password" class="form-control @error('sign_password') is-invalid @enderror"
                                        name="sign_password" required autocomplete="new-password" Placeholder="Password">
 
@@ -62,22 +68,26 @@
                                     <strong>{{ $message }}</strong>
                                 </div>
                                 @enderror
-                            </div>
 
                         </div>
-
-                        <div class="row justify-content-start mt-4">
-                            <div class="col">
-
-
-                                <button class="btn  mt-5 btn-theme">Submit</button>
-                            </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group ">
+                            <button class="btn btn-submit">Submit</button>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+
+            </form>
         </div>
 
-</div>
+    </div>
 
 @endsection
+
+<style>
+    .form-horizontal .form-group {
+        margin-right: 0!important;
+        margin-left: 0!important;
+    }
+</style>
